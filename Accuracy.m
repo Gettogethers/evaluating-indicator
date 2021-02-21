556228837b76b9fa00c7fe78177d2c8538fccf3e
Gettogethers <47889622+Gettogethers@users.noreply.github.com>
2021-02-21 10:02:01 +0800

    Create Accuracy.m

---
+++ Accuracy.m
@@ -0,0 +1,28 @@
+function Accuracy = Accuracy(PredictOutputLable, TureLable)
+% % % %  % % %Accuracy 此处显示有关此函数的摘要 % % %
+% Introduction: Input: PredictOutputLable 分类器的预测标签
+%                      TureLable 待测样本的真实标签
+%               Output：Accuracy 较高时则能说明试验方法预测准确度高
+% Write Time ： 18/10/5
+% 
+% 
+% 
+% ****************************************************************
+[TureLabelRow, ~] = size(TureLabel);
+PostiveIndex = find(TureLable == 1);
+NegativeIndex = find(TureLable == 0);
+
+PostiveNum = find(TureLable == 1);
+NegativeNum = find(TureLable == 0);
+
+TurePostive = find(TureLable(PostiveIndex, :) == PredictOutputLable(PostiveIndex, :));
+TurePostiveNum = size(TurePostive, 1);%TP
+% FalseNegativeNum = PostiveNum - TurePostiveNum;%FN
+
+TureNegative = find(TureLable(NegativeIndex, :) == PredictOutputLable(NegativeIndex, :));
+TureNegativeNum = size(TureNegative, 1);%TN
+% FalsePostiveNum = NegativeNum - TureNegativeNum;%FP
+
+Accuracy = (TurePostiveNum + TureNegativeNum)/ TureLabelRow;
+end
+
